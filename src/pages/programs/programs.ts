36fcb72a78929fb655809542b1553c1c4fa06196
Nav Dessai <ndessai@gmail.com>
2017-09-08 16:17:02 -0400

    added programs page

--- src/pages/programs/programs.ts
+++ src/pages/programs/programs.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ContactusPage } from '../contactus/contactus';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the ProgramsPage page.
@@ -20,6 +22,15 @@ export class ProgramsPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProgramsPage');
+  }
+
+
+  openContactUsPage() {
+    this.navCtrl.setRoot(ContactusPage);
+  }
+
+  openHomePage() {
+    this.navCtrl.setRoot(HomePage);
   }
 
 }
